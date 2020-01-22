5ddc63376a8310417c88b7050acef32e32aec50b
User <user@Student.local>
2020-01-22 15:44:08 +0900

    second commit 2

--- main.tf
+++ main.tf
@@ -13,5 +13,6 @@ resource "aws_instance" "hello-tf-instance" {
   ami = var.ami
   count = var.hello_tf_instance_count
   instance_type = var.hello_tf_instance_type
+  subnet_id = "subnet-01bf3fa56d7711b52"
 }
 
